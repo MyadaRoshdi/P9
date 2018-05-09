152a44a53d7c8bc0839cd1e49b6b962bb420840a
Myada Roshdi <roshdy.miada@gmail.com>
2018-05-09 13:50:29 -0400

    Twiddle() error fix

--- src/PID.cpp
+++ src/PID.cpp
@@ -24,7 +24,7 @@ void PID::UpdateError(double cte) {
 	p_error = cte; // update the Proportional error as the current cte
 	i_error += cte; // update the Integral error as the accumaled cte among all steps call
 	// TODO: USE TWIDDLE ALGORITHM TO ADJUST THE GAIN VALUES (kp,kd,ki)
-	Twiddle(cte);
+	Twiddle(0.2);
 }
 
 double PID::TotalError() {
@@ -35,8 +35,8 @@ double PID::TotalError() {
 
 void PID::Twiddle(double tol) {
 	//Using Twiddle Algorithm to adjust the gain values (Kp,Kd,Ki)
-	double p = [Kp, Kd, Ki];
-	double dp = [1.0, 1.0, 1.0];
+	double p = { Kp, Kd, Ki };
+	double dp = { 1.0, 1.0, 1.0 };
 	double best_err = TotalError();
 	double err = 0.0;
 	
