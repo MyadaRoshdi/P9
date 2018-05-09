676a0a968babbf6628405ae2064f3954cd11f9ab
Myada Roshdi <roshdy.miada@gmail.com>
2018-05-09 14:04:10 -0400

    covert steering angle from degree to radian value

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,8 @@ std::string hasData(std::string s) {
   return "";
 }
 
+
+
 int main()
 {
   uWS::Hub h;
@@ -35,7 +37,7 @@ int main()
   PID pid;
   // Initialize the pid variable.
   pid.Init(0.0, 0.0, 0.0);
- 
+  
  
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
@@ -65,7 +67,7 @@ int main()
 
 		  // Calcuate steering value here, remember the steering value is [-1, 1].
 
-		  steer_value = pid.TotalError();
+		  steer_value = deg2rad(pid.TotalError());
 
           
           // DEBUG
