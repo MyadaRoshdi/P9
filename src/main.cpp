50df7b720daa10fce93c36bfddb756c5c15ce14a
Myada Roshdi <30698281+MyadaRoshdi@users.noreply.github.com>
2018-09-15 10:00:12 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include "PID.h"
 #include <math.h>
 
-using namespace std;
 // for convenience
 using json = nlohmann::json;
 
@@ -29,20 +28,23 @@ std::string hasData(std::string s) {
   return "";
 }
 
-
-
 int main()
 {
   uWS::Hub h;
 
-  PID pid;	
-  //PID pid_steer, pid_throttle;
-  // Initialize the pid variable.
-  //pid_steer.Init(0.12, 0.01, 3.5);
-  //id_throttle.Init(0.45, 0.0000001, 0.5);
-  pid.Init(0.15, 0.0, 2.5);	  
-  
- 
+  PID pid;
+  // TODO: Initialize the pid variable.
+  // Only proportional.
+  // pid.Init(1, 0.0, 0.0);
+
+  // Only integral.
+  // pid.Init(0.0, 1.0, 0.0);
+
+  // Only differential.
+  // pid.Init(0.0, 0.0, 1.0);
+
+  // Final parameters.
+  pid.Init(0.15, 0.0, 2.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,35 +59,22 @@ int main()
         if (event == "telemetry") {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          //double steer_value, throttle_value = 0.8,min_throttle = 0.3, max_throttle = 1.0;
-	  double steer_value = 0.0;	
+          // double speed = std::stod(j[1]["speed"].get<std::string>());
+          // double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          double steer_value = 0.0;
           /*
-          *  Calcuate steering value here, remember the steering value is
+          * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-		  
-		  pid.UpdateError(cte); // Adjust the gains value based on the current cte
-
-		  // Calcuate steering value here, remember the steering value is [-1, 1].
-
-		 steer_value -= pid.TotalError();
-		 //steer_value = deg2rad(steer_value);
-		// steer_value = min(1.0, steer_value); // limit steer_value not more than 1
-		// steer_value = max(steer_value, -1.0); // limit steer_value not less than -1
-		// pid_throttle.UpdateError(fabs(steer_value));
-		 //throttle_value = max_throttle - pid_throttle.Kp * pid_throttle.p_error - pid_throttle.Ki * pid_throttle.i_error - pid_throttle.Kd * pid_throttle.d_error;
-
-		 
+          pid.UpdateError(cte);
+          steer_value -= pid.TotalError();
 
 
-          
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl ;
-
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          std::cout << "Average Error : " <<  pid.AverageError() << " [" << pid.MinError() << ", " << pid.MaxError() << "]"<< std::endl;
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = 0.3;
@@ -120,11 +109,11 @@ int main()
     std::cout << "Connected!!!" << std::endl;
   });
 
-
   h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
     ws.close();
     std::cout << "Disconnected" << std::endl;
   });
+
   int port = 4567;
   if (h.listen(port))
   {
