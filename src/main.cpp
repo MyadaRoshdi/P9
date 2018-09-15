c49e8acf9d253aab5a730f5e36ee766dc9157e7b
Myada Roshdi <30698281+MyadaRoshdi@users.noreply.github.com>
2018-09-15 10:19:58 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -74,7 +74,6 @@ int main()
 
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
-          std::cout << "Average Error : " <<  pid.AverageError() << std::endl;
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = 0.3;
